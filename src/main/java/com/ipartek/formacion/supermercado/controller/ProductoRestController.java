2675821613aa55922c49f9eef3131d93c0ead7b7
AnderSolana <valearangu@hotmail.com>
2020-01-21 14:00:09 +0100

    primet commit

---
+++ src/main/java/com/ipartek/formacion/supermercado/controller/ProductoRestController.java
@@ -0,0 +1,102 @@
+package com.ipartek.formacion.supermercado.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.ipartek.formacion.supermercado.modelo.dao.ProductoDAO;
+import com.ipartek.formacion.supermercado.modelo.pojo.Producto;
+
+/**
+ * Servlet implementation class ProductoRestController
+ */
+@WebServlet({ "/producto/*" })
+public class ProductoRestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private final static Logger LOG = Logger.getLogger(ProductoRestController.class);
+
+	private ProductoDAO productoDao;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		productoDao = ProductoDAO.getInstance();
+	}
+
+	@Override
+	public void destroy() {
+		productoDao = null;
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		super.service(request, response); // llama a doGet, doPost, doPut o doDelete
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("application/json"); // por defecto => text/html;charset=UTF-8
+		response.setCharacterEncoding("utf-8");
+
+		ArrayList<Producto> lista = (ArrayList<Producto>) productoDao.getAll();
+
+		String pathInfo = request.getPathInfo();
+
+		LOG.debug("mirar pathInfo:" + pathInfo + " para saber si es listado o detalle");
+
+		// response body
+		PrintWriter out = response.getWriter();
+		String jsonResponseBody = new Gson().toJson(lista);
+		out.print(jsonResponseBody.toString()); // retornamos un array vacio dentro del body
+		out.flush();
+
+		response.setStatus(HttpServletResponse.SC_OK);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,35 +1,10 @@
-<%@ page contentType="text/html; charset=UTF-8" %>
-
-<%@ include file="/includes/header.jsp" %>
-
-	
-        <div class="row contenedor-productos">
-        	<p>${categorias}</p>
-			<c:forEach items="${productos}" var="producto">
-		            <div class="col">
-		
-		                <!-- producto -->
-		                <div class="producto">
-		                    <span class="descuento">${producto.descuento}</span>
-		                    <img src= ${producto.imagen} alt="imagen de bottela de vodka">
-		
-		                    <div class="body">
-		                        <p>
-		                            <span class="precio-descuento"><fmt:formatNumber type="currency" currencySymbol="€" value="${producto.precio}" /></span>
-		                            <span class="precio-original"><fmt:formatNumber type="currency" currencySymbol="€" value="${producto.precioCalculado()}" /></span>
-		                        </p>
-		                        <p class="text-muted precio-unidad"></p>
-		                        <p class="descripcion text-truncate" >${producto.nombre }</p>
-		                        <p class="descripcion text-truncate" >Creado por: ${producto.usuario.nombre }</p>
-		                        <p class="descripcion text-truncate" >Categoria:  ${producto.categoria.nombre }</p>
-		                    </div> 
-	                    </div>
-		                <!-- /.producto -->
-		
-		            </div>            
-			</c:forEach>
-            
-        </div>
-
-   <%@ include file="/includes/footer.jsp" %>
+<h1>Documentacion</h1>
    
+<h2>Productos</h2>
+
+<ul>
+	<li>Listado Productos: <a href="http://localhost:8080/supermercado-rest/producto/">GET /producto/</a></li>
+	<li>Detalle Producto: <a href="http://localhost:8080/supermercado-rest/producto/1">GET /producto/{1}</a></li>	
+</ul>
+
+<p>El resto de metodos (DELETE, POST Y PUT) hay que probarlos desde Postman o JavaScript</p>
